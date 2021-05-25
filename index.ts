b54e7468169c05aaa17e0c09a4550238133c9520
Ilshat Ishdavletov <lonlyd@gmail.com>
2021-05-25 21:27:22 +0500

    Created a method for result file

--- index.ts
+++ index.ts
@@ -8,13 +8,20 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question("Where is your file? (for example /home/documents/text.txt) \n", (answer) => {
+rl.question("Where is your file? (for example, /home/documents/text.txt or C:/file.txt) \n", (answer) => {
     let filepath: string = path.normalize(answer);
     console.log(filepath);
-    let file: any = fs.readFile(filepath, "utf8", (err: any, data: any) => {
+    let file: any = fs.readFile(filepath, 'utf8', (err: any, data: any) => {
         if (err) throw err;
         console.log(data)
-    });
 
+
+
+
+    });
+    fs.writeFile('result.txt', file, 'utf8', (err) => {
+        if (err) throw  err;
+        console.log('The result file has been saved! Check it!');
+    } )
 });
 
