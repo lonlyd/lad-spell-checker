aabfb071ade3f41819a0da3cd815a03440f29282
Ilshat Ishdavletov <lonlyd@gmail.com>
2021-05-28 11:34:23 +0500

    Create CLI app for spellchecking in file with Yandex speller

--- index.ts
+++ index.ts
@@ -1,27 +1,53 @@
 import fs from 'fs';
 import readline from 'readline';
 import path from 'path';
-import http from 'http';
+import https from 'https';
+const HOST = 'https://speller.yandex.net/services/spellservice.json/checkText?text='
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question("Where is your file? (for example, /home/documents/text.txt or C:/file.txt) \n", (answer) => {
-    let filepath: string = path.normalize(answer);
-    console.log(filepath);
-    let file: any = fs.readFile(filepath, 'utf8', (err: any, data: any) => {
-        if (err) throw err;
-        console.log(data)
+function replaceText(text: any, recievedText: any): any {
+    let textToArray: string[] = text.split(' ');
+    for (let obj of recievedText) {
+        let index: number = textToArray.indexOf(obj.word);
+        textToArray[index] = obj.s[0];
+    }
+    let result: string = textToArray.join(' ');
+    return result;
+}
 
-
-
-
+(function speller(): any {
+    rl.question("Where is your file? (for example, /home/documents/text.txt or C:/file.txt)\n", (answer: any) => {
+        let filepath: string = path.normalize(answer);
+        fs.access(filepath, (err: any) => {
+            if (err) {
+                console.error(`ERROR: No such file or directory, open '${filepath}'`);
+                speller();
+            } else {
+                let readStream: any = fs.createReadStream(filepath, 'utf8');
+                let writeStream: any = fs.createWriteStream(__dirname + '/result.txt');
+                readStream.on('data', (chunk: string) => {
+                    let text: string = chunk.toString();
+                    https.get(HOST + text, (res: any) => {
+                        let recievedText: string;
+                        res.on('data', (d: any) => {
+                            recievedText = JSON.parse(d);
+                            let resultText: string = replaceText(chunk, recievedText);
+                            writeStream.write(resultText);
+                            console.log('The result file has been saved! Check it!');
+                            speller();
+                        });
+                        res.on('error', (error: any) => {
+                            console.error(error);
+                        })
+                    }).on('error', (e: any) => {
+                        console.error(e);
+                    })
+                });
+            }
+        });
     });
-    fs.writeFile('result.txt', file, 'utf8', (err) => {
-        if (err) throw  err;
-        console.log('The result file has been saved! Check it!');
-    } )
-});
-
+})()
